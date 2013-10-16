ba30d33c96020650261addb6524aa6fb2a623cfa
Adam Vinsant <agvinsant@fullsail.edu>
2013-10-16 02:37:16 -0400

    added layout file and webclass jar file getting main functionality started

--- week3/Java1week3/src/com/agvinsant/java1week3/MainActivity.java
+++ week3/Java1week3/src/com/agvinsant/java1week3/MainActivity.java
@@ -10,15 +10,68 @@
  */
 package com.agvinsant.java1week3;
 
+
+import com.agvinsant.lib.BasicLayout;
+import com.agvinsant.webClass.WebClass;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 public class MainActivity extends Activity {
 
+	Context context;
+	Resources res;
+	TextView connectedView;
+	
+	Boolean connected = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// setting the context
+		context = this;
+		res = getResources();
+		
+		
+		// setting the linear layout
+		LinearLayout ll = new LinearLayout(this);
+		LinearLayout ml = BasicLayout.layoutWithButton(this, "Search");
+		ll.setOrientation(LinearLayout.VERTICAL);
+		LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+		ll.setLayoutParams(lp);
+
+		// Creating button from BasicLayout class
+		Button mb = (Button) ml.findViewById(1);
+		
+		connectedView = new TextView(context);
+		
+		TextView headView = new TextView(context);
+		headView.setText("Select a song from the list to see the info");
+		
+		
+		//Detecting network settings
+		connected = WebClass.getConnectionStatus(context);
+		if(connected){
+			Log.i("Network Connection", WebClass.getConnectionType(context));
+			
+			connectedView.setText("Network Connection: " + WebClass.getConnectionType(context)+"\n");
+			
+			// calling the getSongInfo function 
+			//getSongInfo();
+		}
+		else{
+				connectedView.setText(""+ WebClass.getConnectionType(context)+"\n");
+		}
+
+				
 		
 	}
 
---
+++ week3/Java1week3/src/com/agvinsant/lib/BasicLayout.java
@@ -0,0 +1,25 @@
+package com.agvinsant.lib;
+
+import android.content.Context;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class BasicLayout {
+	// creating the linear layout
+    public static LinearLayout layoutWithButton(Context context, String buttonTxt){
+            
+            // setting the linear layout
+            LinearLayout ll = new LinearLayout(context);
+            LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+            ll.setLayoutParams(lp);
+            
+            //Main button created
+            Button button = new Button(context);
+            button.setText(buttonTxt);
+            button.setId(1);
+            ll.addView(button);
+            
+            return ll;
+    }        
+}
